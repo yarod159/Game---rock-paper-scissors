404b3b5c29e7398d7c503cfb1a65b3b4119b352c
yarod159 <86536012+yarod159@users.noreply.github.com>
2023-03-11 13:34:31 -0300

    function bonotes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let puntoUsuario = 0;
-let puntoPc = 0;
+let puntosUsuario = 0;
+let puntosPc = 0;
 let instricciones = document.querySelector("#instrucciones");
 let contenedorPuntosUsuarios = document.querySelector("#puntos-usuario");
 let contenedorPuntosPC = document.querySelector("#puntos-computadora");
@@ -7,13 +7,60 @@ let mensaje = document.querySelector("#mensaje");
 let contendorGanaPunto = document.querySelector("#gana-punto");
 let contenedorEleccionUsuario = document.querySelector("#eleccion-usuario");
 let contenedorEleccionPc = document.querySelector("#eleccion-computadora");
-let elegiTuArma=document.querySelector("#elegi-tu-arma");
-let botonesArmas=document.querySelectorAll('.arma');
-botonesArmas.forEach(boton=>{
-    boton.addEventListener('click', inciarTurno);
-})
-mensaje.classList.add("disabled")
+let elegiTuArma = document.querySelector("#elegi-tu-arma");
+let botonesArmas = document.querySelectorAll(".arma");
 
-function inciarTurno(e){
-    contenedorPuntosUsuarios.innerHTML=3;
+botonesArmas.forEach((boton) => {
+  boton.addEventListener("click", inciarTurno);
+});
+
+function inciarTurno(e) {
+  let eleccionPC = Math.floor(Math.random() * 3);
+  let eleccionUsusario = e.currentTarget.id;
+  console.log(eleccionUsusario);
+
+  if (eleccionPC === 0) {
+    eleccionPC = "piedra🌍";
+  } else if (eleccionPC === 1) {
+    eleccionPC = "papel📋";
+  } else if(eleccionPC===2) {
+    eleccionPC = "tijera✂";
+  }
+  console.log(eleccionPC);
+
+  if (
+    (eleccionUsusario === "piedra🌍" && eleccionPC === "tijera✂") ||
+    (eleccionUsusario === "papel📋" && eleccionPC === "piedra🌍") ||
+    (eleccionUsusario === "tijera✂" && eleccionPC === "papel📋")
+  ) {
+    ganaUsuario();
+  } else if (
+    (eleccionPC === "piedra🌍" && eleccionUsusario === "tijera✂") ||
+    (eleccionPC === "papel📋" && eleccionUsusario === "piedra🌍") ||
+    (eleccionPC === "tijera✂" && eleccionUsusario === "papel📋")
+  ) {
+    ganaPC();
+  } else {
+    empate();
+  }
+  
+}
+
+function ganaPC(){
+  puntosPc++;
+  contenedorPuntosPC.innerText=puntosPc;
+  contendorGanaPunto.innerText="la computadora gano un punto";
+  console.log(contendorGanaPunto);
+}
+
+function ganaUsuario(){
+    puntosUsuario++;
+    contenedorPuntosUsuarios.innerText=puntosUsuario;
+    contendorGanaPunto.innertext="ganaste un punto ";
+    console.log(contendorGanaPunto);
+   
+}
+
+function empate(){
+    contendorGanaPunto.innertext="empate";
 }
